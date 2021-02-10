aca6b921b79b7dbcf4415e157f8d892c54564e16
Andrea Reni <mail@nbl7.net>
2021-02-10 13:44:10 +0100

    status: running. web book, core book, logger.

--- src/book.ts
+++ src/book.ts
@@ -1,142 +1,10 @@
 
 import * as book_types from './types';
 
-const core_atoms_book = {
-	debug: {
-		connection: 'log',
-		security: {
-			type: book_types.BookSecurityType.UNIFORM,
-			_r: null
-		},
-		api:{
-			url: '/log-debug'
-		},
-		properties: {
-			msg: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'Message'
-			},
-			body: {
-				type: book_types.BookPropertyType.LONG_TEXT,
-				label: 'Body'
-			},
-			path: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'Path'
-			},
-			ip: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'IP',
-			},
-			params: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'Params'
-			},
-			query: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'Query'
-			}
-		}
-	},
-	error: {
-		connection: 'log',
-		security: {
-			type: book_types.BookSecurityType.UNIFORM,
-			_r: null
-		},
-		api:{
-			url: '/log-error'
-		},
-		properties: {
-			msg: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'Message'
-			},
-			body: {
-				type: book_types.BookPropertyType.LONG_TEXT,
-				label: 'Body'
-			},
-			path: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'Path'
-			},
-			ip: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'IP',
-			},
-			params: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'Params'
-			},
-			query: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'Query'
-			}
-		}
-	},
-	superuser: {
-		security: {
-			type: book_types.BookSecurityType.UNIFORM,
-			_r: null
-		},
-		api:{
-			url: '/superusers'
-		},
-		properties: {
-			email: {
-				type: book_types.BookPropertyType.EMAIL,
-				label: 'Email',
-				unique: true,
-				on_error: () => {
-					return 'email@email.com';
-				}
-			},
-			password: {
-				type: book_types.BookPropertyType.ENCRYPTED,
-				label: 'Password'
-			}
-		}
-	},
-	user: {
-		securiy: {
-			type: book_types.BookSecurityType.GRANULAR
-		},
-		api:{
-			url: '/users'
-		},
-		properties: {
-			email: {
-				type: book_types.BookPropertyType.EMAIL,
-				label: 'Email',
-				unique: true,
-			},
-			password: {
-				type: book_types.BookPropertyType.ENCRYPTED,
-				label: 'Password'
-			},
-			groups: {
-				type: book_types.BookPropertyType.ATOM_ARRAY,
-				atom: 'group',
-				label: 'Groups',
-				optional: true
-			}
-		}
-	},
-	group: {
-		api:{
-			url: '/groups'
-		},
-		properties: {
-			name: {
-				type: book_types.BookPropertyType.TEXT,
-				label: 'Name'
-			}
-		}
-	}
-} as const;
+import {web_atom_book} from 'urn_web/book';
 
-export const atom_book = {
-	...core_atoms_book,
+export const dev_atom_book = {
+	...web_atom_book,
 	media: {
 		api: {
 			url: '/medias'
@@ -171,4 +39,8 @@ export const atom_book = {
 			}
 		}
 	}
+} as const;
+
+export const atom_book = {
+	...dev_atom_book
 } as const;
