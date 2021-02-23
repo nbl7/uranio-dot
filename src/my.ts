5fed04063a810aaab34a694b84de80cea61d9951
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-02-23 10:28:01 +0100

    started custom routes and custom bll.

---
+++ src/my.ts
@@ -0,0 +1,25 @@
+
+// import {BLL} from './urn-core/bll/bll';
+
+// import {
+//   TokenObject,
+//   Depth,
+//   Molecule
+// } from './urn-core/types';
+
+// export class my_class extends BLL<'product'> {
+//   constructor(token_object?:TokenObject){
+//     super('product', token_object);
+//   }
+//   public async find_by_id<D extends Depth>(id:string,options:unknown)
+//       :Promise<Molecule<'product', D>>{
+//     console.log('AAAAAAAAAAAAAAAAAAAAA',id, options);
+//     return await super.find_by_id(id) as Molecule<'product',D>;
+//   }
+// }
+
+// export function my_bll(token_object:TokenObject)
+//       :BLL<'product'>{
+//   return new my_class(token_object);
+// }
+
