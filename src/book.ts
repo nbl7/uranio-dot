60058b37268d547d4255d0e4414fbad8660e6e9a
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-02-28 13:33:58 +0100

    tsconfig target es5, testing generating atom_book from user defined.

--- src/book.ts
+++ src/book.ts
@@ -74,3 +74,6 @@ export const atom_book:uranio.types.Book = {
 		}
 	},
 };
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,55 @@
  * @packageDocumentation
  */
 
+// import fs from 'fs';
+
+// import {atom_book} from './book';
+
+// const stringified = JSON.stringify(atom_book, (_, v) => {
+//   return (typeof v === 'function' ) ? v.toString() : v;
+// }, '\t');
+
+// fs.writeFileSync(`${__dirname}/../.urn/nbook.ts`, stringified);
+
+// console.log(__dirname);
+
+// const read = fs.readFileSync(`${__dirname}/book.js`, 'utf8');
+
+// const splitted = read.split('exports.atom_book = {');
+// const part = splitted[1];
+
+// let count_brackets = 0;
+// let i = 0;
+// for(i = 0; i < part.length; i++){
+//   const char = part[i];
+//   if(char !== '{' && char !== '}'){
+//     continue;
+//   }
+//   if(char === '{'){
+//     count_brackets++;
+//   }
+//   if(char === '}'){
+//     count_brackets--;
+//   }
+//   if(count_brackets === -1){
+//     break;
+//   }
+// }
+
+// const atom_book = `{${part.slice(0,i+1)}`;
+// console.log(atom_book);
+
+
+
+// import ts from "typescript";
+
+// const source = "let x: string  = 'string'";
+
+// const result = ts.transpileModule(source, { compilerOptions: { module: ts.ModuleKind.CommonJS }});
+
+// console.log(JSON.stringify(result));
+
+
 // import {BLL} from './urn-core/bll/bll';
 
 // import {
