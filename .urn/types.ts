550920c22035ec66592088663615749ab318468b
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-03-06 09:30:21 +0100

    trying to solve circular dep.

---
+++ .urn/types.ts
@@ -0,0 +1,19 @@
+import urn_core from './urn-core/';
+
+import urn_web from './urn-web/';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export import BookSecurityType = urn_core.types.BookSecurityType;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export import BookPropertyType = urn_core.types.BookPropertyType;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export import BookPermissionType = urn_core.types.BookPermissionType;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export import  AuthAction = urn_core.types.AuthAction;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export import  RouteMethod = urn_web.types.RouteMethod;
+
--- src/myprodbll.ts
+++ src/myprodbll.ts
@@ -2,7 +2,18 @@
 import uranio from 'uranio';
 
 export class my_prod_bll extends uranio.core.bll.BLL<'product'>{
-	constructor(to:uranio.types.TokenObject){
+	constructor(to?:uranio.types.TokenObject){
 		super('product', to);
 	}
+	public async find<D extends uranio.types.Depth>(query:uranio.types.Query<'product'>, options?:uranio.types.Query.Options<'product',D>)
+			:Promise<uranio.types.Molecule<'product',D>[]>{
+		console.warn('kdajflkjdfAJFHDKSHFLKSDHFLKSDHFLKHSDLKFHDSKLHFLSDKHFLKSDHFLKSHDFKLSDH');
+		return await this._al.select(query, options);
+	}
+}
+
+export function return_bll(token_object?:uranio.types.TokenObject)
+		:uranio.core.bll.BLL<'product'>{
+		
+	return new my_prod_bll(token_object);
 }
