26541879e57e8483e896a326d2c13eb008f9eb85
Andrea Reni <mail@nbl7.net>
2021-03-07 17:33:57 +0100

    fixed uranio alias. src index.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,14 @@
  * @packageDocumentation
  */
 
+import {urn_log} from 'urn-lib';
+
+urn_log.defaults.log_level = urn_log.LogLevel.FUNCTION_DEBUG;
+
+import uranio from 'uranio';
+
+const express_service = uranio.service.express.create();
+
+express_service.listen(3000, () => {
+	urn_log.debug(`Listening on port 3000...`);
+});
