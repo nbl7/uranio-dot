0372aaa7be3877a345a18c0cf1aa9d7c05239f33
Andrea Reni <mail@nbl7.net>
2021-03-29 16:11:39 +0200

    ServiceName in config file.

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import uranio from 'uranio';
 
 uranio.lib.log.defaults.log_level = uranio.lib.log.LogLevel.FUNCTION_DEBUG;
 
-const express_service = uranio.service.express.create();
+const express_service = uranio.service.create();
 
 express_service.listen(3000, () => {
 	uranio.lib.log.debug(`Listening on port 3000...`);
