e1c6f84f73c9d054d4e8aa7ec7c7be3a26e51708
Andrea Reni <mail@nbl7.net>
2021-04-12 19:07:25 +0200

    removed core.

--- switch.js
+++ switch.js
@@ -14,10 +14,10 @@ if(fs.existsSync(json_filepath)){
 	const urnsub = JSON.parse(content);
 	const current_submodule = urnsub.submodule;
 	if(typeof current_submodule === 'string'){
-		_execute(`git submodule deinit ${current_submodule}`);
-		_execute(`git rm ${current_submodule}`);
-		_execute(`rm -rf ${current_submodule}`);
-		_execute(`rm -rf ../.git/modules/urn-dot/modules/${current_submodule}`);
+		_execute(`git submodule deinit .uranio/repo`);
+		_execute(`git rm .uranio/repo`);
+		_execute(`rm -rf .uranio/repo`);
+		_execute(`rm -rf ../.git/modules/urn-dot/modules/.uranio/repo`);
 		_execute('git add .');
 		_execute(`git commit -m "[removed submodule ${current_submodule}]"`);
 	}
@@ -25,18 +25,18 @@ if(fs.existsSync(json_filepath)){
 	_execute(`touch ${json_filepath}`);
 }
 
-const urn_repo = args._[0];
+const selected_repo = args._[0];
 // const dest = args._[1] || args._[0];
 
-_execute(`git submodule add -b master git+ssh://git@bitbucket.org/nbl7/urn-${urn_repo} .uranio/${urn_repo}`);
-_execute(`git config -f .gitmodules submodule..uranio/${urn_repo}.update rebase`);
+_execute(`git submodule add -b master git+ssh://git@bitbucket.org/nbl7/urn-${selected_repo} .uranio/repo`);
+_execute(`git config -f .gitmodules submodule..uranio/repo.update rebase`);
 _execute(`git submodule update --remote`);
 
-const urnsub = {submodule: `.uranio/${urn_repo}`};
+const urnsub = {submodule: `${selected_repo}`};
 fs.writeFileSync(json_filepath, JSON.stringify(urnsub) + '\n');
 
 _execute('git add .');
-_execute(`git commit -m "[added submodule ${urn_repo}]"`);
+_execute(`git commit -m "[added submodule ${selected_repo}]"`);
 
 function _execute(command){
 	console.log(command);
