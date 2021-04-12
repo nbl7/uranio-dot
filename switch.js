971b97bac38abecf624f4d8ef78cfbc42faacdd7
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-04-12 17:29:59 +0200

    removed all submodule from .uranio.

--- switch.js
+++ switch.js
@@ -14,18 +14,48 @@ if(fs.existsSync(json_filepath)){
 	const urnsub = JSON.parse(content);
 	const current_submodule = urnsub.submodule;
 	if(typeof current_submodule === 'string'){
-		cp.execSync(`git submodule deinit ${current_submodule}`);
-		cp.execSync(`git rm ${current_submodule}`);
-		cp.execSync(`git commit -m "removed submodule ${current_submodule}"`);
-		cp.execSync(`rm -rf ../.git/modules/urn-dot/modules/${current_submodule}`);
+		_spawn(`git submodule deinit ${current_submodule}`);
+		_spawn(`git rm ${current_submodule}`);
+		_spawn(`git commit -m "removed submodule ${current_submodule}"`);
+		_spawn(`rm -rf ../.git/modules/urn-dot/modules/${current_submodule}`);
 	}
+}else{
+	_spawn(`touch ${json_filepath}`);
 }
 
 const urn_repo = args._[0];
 // const dest = args._[1] || args._[0];
 
-cp.execSync(`git submodule add -b master git+ssh://git@bitbucket.org/nbl7/urn-${urn_repo} .uranio/${urn_repo})`);
-cp.execSync(`git config -f .gitmodule submodule..uranio/${urn_repo}.update rebase`);
-cp.execSync(`git submodule update --remote`);
+_spawn(`git submodule add -b master git+ssh://git@bitbucket.org/nbl7/urn-${urn_repo} .uranio/${urn_repo}`);
+_spawn(`git config -f .gitmodules submodule..uranio/${urn_repo}.update rebase`);
+_spawn(`git submodule update --remote`);
 
+const urnsub = {submodule: `.uranio/${urn_repo}`};
+fs.writeFileSync(json_filepath, JSON.stringify(urnsub));
 
+function _spawn(command){
+	console.log(command);
+	const splitted = command.split(' ');
+	const first_cmd = splitted[0];
+	splitted.shift();
+	const child = cp.spawn(first_cmd, splitted);
+	
+	if(child.stdout){
+		child.stdout.setEncoding('utf8');
+		child.stdout.on('data', (chunk) => {
+			console.log(chunk);
+		});
+	}
+	
+	if(child.stderr){
+		child.stderr.setEncoding('utf8');
+		child.stderr.on('data', (chunk) => {
+			console.log(chunk);
+		});
+	}
+	
+	child.on('error', (err) => {
+		console.error(err);
+	});
+
+}
