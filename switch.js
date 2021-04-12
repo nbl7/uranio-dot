10f04d6bb21eeb364c3836e4e63fc3abde290a13
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-04-12 17:51:42 +0200

    added web

--- switch.js
+++ switch.js
@@ -17,9 +17,9 @@ if(fs.existsSync(json_filepath)){
 		_spawn(`git submodule deinit ${current_submodule}`);
 		_spawn(`git rm ${current_submodule}`);
 		_spawn(`rm -rf ${current_submodule}`);
+		_spawn(`rm -rf ../.git/modules/urn-dot/modules/${current_submodule}`);
 		_spawn('git add .');
 		_spawn(`git commit -m "[removed submodule ${current_submodule}]"`);
-		_spawn(`rm -rf ../.git/modules/urn-dot/modules/${current_submodule}`);
 	}
 }else{
 	_spawn(`touch ${json_filepath}`);
