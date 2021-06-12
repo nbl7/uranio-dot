1a1f4db06852fcb8a43ee55724f74a86cdd1106c
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-06-12 16:40:49 +0200

    added check on working directory before proceeding.

--- switch.js
+++ switch.js
@@ -5,46 +5,61 @@ const minimist = require('minimist');
 const cp = require('child_process');
 const fs = require('fs');
 
-const args = minimist(process.argv.slice(2));
-
-const selected_repo = args._[0];
-const valid_repos = ['core', 'web', 'fnc'];
-
-if(!valid_repos.includes(selected_repo)){
-	console.log('Invalid repo argument.');
-	process.exit(1);
-}
-
-const json_filepath = `urnsub.json`;
-
-if(fs.existsSync(json_filepath)){
-	const content = fs.readFileSync(json_filepath, {encoding: 'utf8'});
-	const urnsub = JSON.parse(content);
-	const current_submodule = urnsub.submodule;
-	if(typeof current_submodule === 'string'){
-		_execute(`git submodule deinit .uranio/repo`);
-		_execute(`git rm .uranio/repo`);
-		_execute(`rm -rf .uranio/repo`);
-		_execute(`rm -rf ../.git/modules/urn-dot/modules/.uranio/repo`);
-		_execute('git add .');
-		_execute(`git commit -m "[removed submodule ${current_submodule}]"`);
+const spawn = cp.spawn('git', ['status','--porcelain']);
+spawn.stdout.on('data', function(data){
+	if(data.toString() === ''){
+		_proceed();
+	}else{
+		console.log('-------------------------------------------------------------');
+		console.log('Working directory not clean. Please commit before proceeding.');
+		console.log('-------------------------------------------------------------');
 	}
-}else{
-	_execute(`touch ${json_filepath}`);
+});
+
+function _proceed(){
+	
+	const args = minimist(process.argv.slice(2));
+	
+	const selected_repo = args._[0];
+	const valid_repos = ['core', 'web', 'fnc'];
+	
+	if(!valid_repos.includes(selected_repo)){
+		console.log('Invalid repo argument.');
+		process.exit(1);
+	}
+	
+	const json_filepath = `urnsub.json`;
+	
+	if(fs.existsSync(json_filepath)){
+		const content = fs.readFileSync(json_filepath, {encoding: 'utf8'});
+		const urnsub = JSON.parse(content);
+		const current_submodule = urnsub.submodule;
+		if(typeof current_submodule === 'string'){
+			_execute(`git submodule deinit .uranio/repo`);
+			_execute(`git rm .uranio/repo`);
+			_execute(`rm -rf .uranio/repo`);
+			_execute(`rm -rf ../.git/modules/urn-dot/modules/.uranio/repo`);
+			_execute('git add .');
+			_execute(`git commit -m "[removed submodule ${current_submodule}]"`);
+		}
+	}else{
+		_execute(`touch ${json_filepath}`);
+	}
+	
+	const origin = `git+ssh://git@bitbucket.org/nbl7/urn-${selected_repo}`;
+	
+	_execute(`git submodule add -b master ${origin} .uranio/repo`);
+	_execute(`git config -f .gitmodules submodule..uranio/repo.update rebase`);
+	_execute(`git submodule update --remote --init --recursive`);
+	// _execute(`git submodule update --remote`);
+	
+	const urnsub = {submodule: `${selected_repo}`};
+	fs.writeFileSync(json_filepath, JSON.stringify(urnsub) + '\n');
+	
+	_execute('git add .');
+	_execute(`git commit -m "[added submodule ${selected_repo}]"`);
+	
 }
-
-const origin = `git+ssh://git@bitbucket.org/nbl7/urn-${selected_repo}`;
-
-_execute(`git submodule add -b master ${origin} .uranio/repo`);
-_execute(`git config -f .gitmodules submodule..uranio/repo.update rebase`);
-_execute(`git submodule update --remote --init --recursive`);
-// _execute(`git submodule update --remote`);
-
-const urnsub = {submodule: `${selected_repo}`};
-fs.writeFileSync(json_filepath, JSON.stringify(urnsub) + '\n');
-
-_execute('git add .');
-_execute(`git commit -m "[added submodule ${selected_repo}]"`);
 
 function _execute(command){
 	console.log(command);
