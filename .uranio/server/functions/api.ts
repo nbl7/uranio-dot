8a255a2de97312a5a7a48c8b15416bfc1692c7e2
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-08-20 11:31:07 +0200

    updated books and functions. updated lib trx.

--- .uranio/server/functions/api.ts
+++ .uranio/server/functions/api.ts
@@ -1,7 +1,9 @@
-import uranio from '../../lib/';
-const uranio_lambda = uranio.api.lambda.create();
-const handler = async (event:uranio.api.lambda.LambdaEvent, context:uranio.api.lambda.LambdaContext)
-		:Promise<uranio.api.lambda.HandlerResponse> => {
+import * as lambda_12113241 from "../../lib/api/lambda";
+const uranio_lambda = lambda_12113241.create();
+const handler = async (
+	event: lambda_12113241.LambdaEvent,
+	context: lambda_12113241.LambdaContext
+): Promise<lambda_12113241.HandlerResponse> => {
 	return await uranio_lambda.handle(event, context);
 };
 export { handler };
