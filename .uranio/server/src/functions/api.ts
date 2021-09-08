a6207c241653751031085f90042dd624fd63aa15
Andrea Reni <mail@nbl7.net>
2021-09-08 18:08:55 +0200

    fixed relative path for server src files.

--- .uranio/server/src/functions/api.ts
+++ .uranio/server/src/functions/api.ts
@@ -1,4 +1,4 @@
-import * as lambda_12113241 from "../../lib/api/lambda";
+import * as lambda_12113241 from "../lib/api/lambda";
 const uranio_lambda = lambda_12113241.create();
 const handler = async (
 	event: lambda_12113241.LambdaEvent,
