26d7aecfefd763bed97bcb01a25dd5f6c85c63a3
Andrea Reni <mail@nbl7.net>
2021-09-12 18:02:20 +0200

    edited readme. changed client tsconfig. changed nuxt config, netifly config, package.json.

--- admin/nuxt.config.js
+++ admin/nuxt.config.js
@@ -2,9 +2,12 @@ import { resolve } from 'path';
 
 export default {
 	alias: {
-		'uranio': resolve(__dirname, '../../.uranio/lib/client'),
-		'uranio-book': resolve(__dirname, '../../.uranio/client/books/atom'),
-		'uranio-client': resolve(__dirname, '../../.uranio/lib/client'),
+		'uranio': resolve(__dirname, './src/uranio/client'),
+		'uranio-api': resolve(__dirname, './src/uranio/api/'),
+		'uranio-core': resolve(__dirname, './src/uranio/api/core/'),
+		'uranio-client': resolve(__dirname, './src/uranio/client'),
+		'uranio-books': resolve(__dirname, './src/books/'),
+		'uranio-books-client': resolve(__dirname, './src/books/'),
 	},
 	components: [
 		{
@@ -12,14 +15,16 @@ export default {
 			extensions: ['vue']
 		}
 	],
-	buildDir: '.uranio/client/.nuxt',
-	srcDir: '.uranio/client/src',
+	buildDir: './.nuxt',
+	srcDir: './src',
 	target: 'static',
 	ssr: false,
 	buildModules: [
 		'@nuxt/typescript-build',
-		'~/modules/mym/index'
 	],
+	typescript: {
+		typeCheck: true
+	},
 	generate: {
 		fallback: '404.html',
 		exclude: ['/urn-admin'],
@@ -30,31 +35,23 @@ export default {
 		linkExactActiveClass: 'urn-exact-active-link'
 	},
 	loading: {
-		color: '#FC2222',
+		color: '#2222FF',
 		height: '2px',
 		throttle: 200,
 		duration: 2000,
 		continuous: true
 	},
-	watchers: {
-		// chokidar: {
-		//   ignored: [
-		//     `${process.cwd()}/.uranio/server/**/*`,
-		//     `${process.cwd()}/src/server/**/*`,
-		//     `${process.cwd()}/node_modules`,
-		//   ]
-		// },
-		webpack: {
-			ignored: [
-				`${process.cwd()}/node_modules/**/*`,
-				`${process.cwd()}/.uranio/server/**/*`,
-				`${process.cwd()}/.uranio/.tmp/**/*`,
-				`${process.cwd()}/src/server/**/*`,
-				`${process.cwd()}/src/book.ts`,
-				`${process.cwd()}/dist/**/*`,
-			]
-		}
-	},
+	// watchers: {
+	//   webpack: {
+	//     ignored: [
+	//       `${process.cwd()}/node_modules/**/*`,
+	//       `${process.cwd()}/.uranio/server/**/*`,
+	//       `${process.cwd()}/.uranio/.tmp/**/*`,
+	//       `${process.cwd()}/src/**/*`,
+	//       `${process.cwd()}/dist/**/*`,
+	//     ]
+	//   }
+	// },
 	hooks: {
 		build: {
 			before(){
