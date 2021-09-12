f08abec524dcd999d15cf1a7367eedf55d431c09
Andrea Reni <mail@nbl7.net>
2021-09-12 18:35:03 +0200

    [removed submodule .uranio/uranio]

--- bin/common.js
+++ bin/common.js
@@ -2,7 +2,7 @@
 
 const cp = require('child_process');
 
-export function deinit(submodule_path){
+function deinit(submodule_path){
 	execute(`git submodule deinit ${submodule_path}`);
 	execute(`git rm ${submodule_path}`);
 	execute(`rm -rf ${submodule_path}`);
@@ -11,14 +11,20 @@ export function deinit(submodule_path){
 	execute(`git commit -m "[removed submodule ${submodule_path}]"`);
 }
 
-export function add_submodule(origin, submodule_path, branch='master'){
+function add_submodule(origin, submodule_path, branch='master'){
 	execute(`git submodule add -b ${branch} ${origin} ${submodule_path}`);
 	execute(`git config -f .gitmodules submodule.${submodule_path}.update rebase`);
 	execute(`git submodule foreach --recursive 'case $displaypath in ".uranio"*) git checkout ${branch} ;; *) : ;; esac'`);
 }
 
-export function execute(command){
+function execute(command){
 	console.log(command);
 	cp.execSync(command);
 }
+
+module.exports = {
+	deinit,
+	add_submodule,
+	execute
+};
 
