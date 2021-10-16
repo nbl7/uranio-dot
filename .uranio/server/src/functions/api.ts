232ecf50af649a124d94a6785b674666007dd163
Andrea Reni <mail@nbl7.net>
2021-10-16 14:21:32 +0200

    added netrlify folder. upadted books. updated netlify functions file. updated package.json.

--- .uranio/server/src/functions/api.ts
+++ .uranio/server/src/functions/api.ts
@@ -1,9 +1,12 @@
-import uranio from 'uranio';
-const uranio_lambda = uranio.api.lambda.create();
+import * as lambda_12113241 from "../uranio/trx/api/lambda";
+const uranio_lambda = lambda_12113241.create();
 const handler = async (
-	event: uranio.api.lambda.LambdaEvent,
-	context: uranio.api.lambda.LambdaContext
-): Promise<uranio.api.lambda.HandlerResponse> => {
+	event: lambda_12113241.LambdaEvent,
+	context: lambda_12113241.LambdaContext
+): Promise<lambda_12113241.HandlerResponse> => {
 	return await uranio_lambda.handle(event, context);
 };
 export { handler };
+
+// uranio autoupdate
+// 1
