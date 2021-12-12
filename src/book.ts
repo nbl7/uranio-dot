6ad16fb62975698d033fb48a52172fd24e78133b
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-12-12 17:41:27 +0100

    removed dock from core. before changing to api.

--- src/book.ts
+++ src/book.ts
@@ -15,30 +15,15 @@ export const atom_book:uranio.types.Book = {
 		},
 		properties: {
 			first_name: {
-				sortable: false,
 				type: uranio.types.BookPropertyType.TEXT,
 				label: 'First name'
 			},
 			last_name: {
 				type: uranio.types.BookPropertyType.TEXT,
 				label: 'Last name',
-				is_title: true
 			}
 		},
 		dock:{
-			url: '/customers',
-			routes:{
-				pippi:{
-					method: uranio.types.RouteMethod.GET,
-					action: uranio.types.AuthAction.READ,
-					url: '/pippo',
-					return: Number,
-					call: async (req:uranio.types.Api.Request<'customer', 'pippi'>):Promise<number> => {
-						console.log(req.route_name);
-						return 899;
-					}
-				}
-			}
 		}
 	},
 	mykart: {
@@ -49,19 +34,12 @@ export const atom_book:uranio.types.Book = {
 		plural: 'mykarts',
 		properties:{
 			title:{
-				is_title: true,
 				type: uranio.types.BookPropertyType.TEXT,
 				// label: `${some}-titless`
 				label: `titless`,
-				style: {
-					full_width: true,
-					classes: 'custom-class-name'
-				}
 			}
 		},
 		dock: {
-			url: '/mykarts',
-			auth: '/myauthkart'
 		},
 		// bll: () => {
 		//   console.log(some + `s`);
@@ -75,13 +53,8 @@ export const atom_book:uranio.types.Book = {
 		},
 		properties: {
 			title: {
-				is_title: true,
-				primary: true,
 				type: uranio.types.BookPropertyType.TEXT,
 				label: 'Title',
-				style:{
-					full_width: true
-				},
 				validation: {
 					alphanum: true,
 					contain_digit: false,
@@ -90,15 +63,10 @@ export const atom_book:uranio.types.Book = {
 				on_error: () => 'TITLE'
 			},
 			description: {
-				primary: true,
 				type: uranio.types.BookPropertyType.LONG_TEXT,
 				label: 'Description',
-				style:{
-					full_width: true
-				}
 			},
 			kart: {
-				primary: true,
 				type: uranio.types.BookPropertyType.ATOM,
 				label: 'Kart',
 				atom: 'mykart',
@@ -111,12 +79,10 @@ export const atom_book:uranio.types.Book = {
 				optional: true
 			},
 			active: {
-				primary: true,
 				type: uranio.types.BookPropertyType.BINARY,
 				label: 'Active',
 			},
 			email: {
-				primary: true,
 				type: uranio.types.BookPropertyType.EMAIL,
 				label: 'Email',
 			},
@@ -125,7 +91,6 @@ export const atom_book:uranio.types.Book = {
 				label: 'Password',
 			},
 			type: {
-				primary: true,
 				type: uranio.types.BookPropertyType.ENUM_NUMBER,
 				label: 'Type Code',
 				values: [1,2,3],
@@ -133,14 +98,12 @@ export const atom_book:uranio.types.Book = {
 				default: 1
 			},
 			type_str: {
-				primary: true,
 				type: uranio.types.BookPropertyType.ENUM_STRING,
 				label: 'Type String',
 				values: ['Red','Green','Blue'],
 				optional: true
 			},
 			price: {
-				primary: true,
 				type: uranio.types.BookPropertyType.FLOAT,
 				label: 'Price',
 				validation: {
@@ -148,7 +111,6 @@ export const atom_book:uranio.types.Book = {
 				}
 			},
 			unit: {
-				primary: true,
 				type: uranio.types.BookPropertyType.INTEGER,
 				label: 'Unit',
 				// validation: {
@@ -179,19 +141,6 @@ export const atom_book:uranio.types.Book = {
 			},
 		},
 		dock: {
-			url: '/products',
-			auth: '/proauths',
-			routes: {
-				myroute: {
-					method: uranio.types.RouteMethod.GET,
-					action: uranio.types.AuthAction.READ,
-					url: '/myroute',
-					call: async (api_request:uranio.types.Api.Request<'product','myroute'>):Promise<number> => {
-						console.log(api_request);
-						return 8;
-					}
-				}
-			}
 		}
 	}
 };
