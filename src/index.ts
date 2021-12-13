0de99e76b5332c630a6ed67c6c75f06db0736258
Andrea Reni <mail@nbl7.net>
2021-12-13 12:44:25 +0100

    first commit api.

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,6 @@ import uranio from 'uranio';
 
 const express_service = uranio.api.service.create();
 
-express_service.listen(8888, () => {
-	urn_log.debug(`Listening on port 8888...`);
-});
-
-uranio.hooks.superusers.count({}).then((trx_response) => {
-	if(trx_response.success === true){
-		const count_superuser = trx_response.payload;
-		urn_log.debug(`Superuser count: ${count_superuser}`);
-	}else{
-		urn_log.debug(trx_response.err_code);
-	}
+express_service.listen(7777, () => {
+	urn_log.debug(`Listening on port 7777...`);
 });
