11c3cb8bc5d1ec33bb40dad0c376fa74103907ee
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-12-17 15:02:48 +0100

    moved to core.

---
+++ src/core.ts
@@ -0,0 +1,16 @@
+/**
+ * URANIO auto-generated file for start developing with uranio-core repo.
+ *
+ * @packageDocumentation
+ */
+
+import {urn_log} from 'urn-lib';
+urn_log.init(urn_log.LogLevel.DEBUG);
+
+import uranio from 'uranio';
+
+const superuser_bll = uranio.bll.basic.create(`superuser`);
+
+superuser_bll.count({}).then((count_number) => {
+	urn_log.debug(count_number);
+});
