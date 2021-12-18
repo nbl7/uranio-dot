a86f50e219c2aff0ebca7fe76e8f58360ca4b07a
Andrea Reni <mail@nbl7.net>
2021-12-18 11:09:19 +0100

    added sample.env. updated submodule src/uranio.

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ urn_log.init(urn_log.LogLevel.DEBUG);
 
 import uranio from 'uranio';
 
-const express_service = uranio.api.service.create();
+const express_service = uranio.service.create();
 
 express_service.listen(7777, () => {
 	urn_log.debug(`Listening on port 7777...`);
